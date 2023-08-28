608711b6177196e5b38177d3e0c9909163ba61fd
Nathan May <nathaniel.may22@gmail.com>
2023-08-28 12:31:41 -0600

    late commit to make updateQuality() immutable

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -17,47 +17,40 @@ export class GildedRose {
     this.items = items;
   }
 
-  updateQuality():Array<Item> {
-    this.items.forEach((item) => {
-
-      const lowerName = item.name.toLowerCase();
+  updateQuality(): Array<Item> {
+    return this.items.map((item) => {
+      const lower = item.name.toLowerCase();
+      let quality = item.quality;
 
       // Sulfuras are unaffected by sellBy or quality
-      if (lowerName.includes("sulfuras")) return;
+      if (lower.includes("sulfuras")) return item;
 
       // is the item past its sellBy date?
       const after = item.sellIn <= 0;
 
-
       // "Aged Brie" actually increases in Quality the older it gets and double after sellBy
-      if (lowerName.includes("brie"))
-        item.quality = safeIncrement(item, after ? 2 : 1);
-      
+      if (lower.includes("brie")) quality = safeIncrement(item, after ? 2 : 1);
 
       // "Backstage passes" increases in Quality at the 10 and 5 day mark and dropd to 0 after sellBy
-      else if (lowerName.includes("backstage pass")) {
-        if (after) item.quality = 0;
-        else if (item.sellIn <= 5) item.quality = safeIncrement(item, 3);
-        else if (item.sellIn <= 10) item.quality = safeIncrement(item, 2);
-        else item.quality = safeIncrement(item, 1);
+      else if (lower.includes("backstage pass")) {
+        if (after) quality = 0;
+        else if (item.sellIn <= 5) quality = safeIncrement(item, 3);
+        else if (item.sellIn <= 10) quality = safeIncrement(item, 2);
+        else quality = safeIncrement(item, 1);
       }
 
       // "Conjured" items degrade in Quality twice as fast as normal items
-      else if (lowerName.includes("conjured"))
-        item.quality = safeDecrement(item, after ? 4 : 2);
-      
-
+      else if (lower.includes("conjured"))
+        quality = safeDecrement(item, after ? 4 : 2);
       // all other items degrade in by 1 before sellBy and 2 after
-      else item.quality = safeDecrement(item, after ? 2 : 1);
+      else quality = safeDecrement(item, after ? 2 : 1);
 
-      // decrement sellIn
-      item.sellIn--;
-
+      return {
+        ...item,
+        sellIn: item.sellIn - 1,
+        quality,
+      };
     });
-
-    
-
-    return this.items;
   }
 }
 
